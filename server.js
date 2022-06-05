e84d83430e7eea7cf2160bbd6c5e685d96afd6e8
Andrei Rezonov <andreirezonov@gmail.com>
2022-06-05 23:36:23 +0200

    modified server.js & Procfile

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-//set port
-
-var port = process.env.port || 8080
-
-app.use(express.static(__dirname));
-
-// routes
-
-app.get("./", function(req, res){
-    res.render("index");
-});
-
-app.listen(port, function(){
-    console.log("")
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
