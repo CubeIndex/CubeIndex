fc0782fbc002b0f76c07364e6ab80d4d2b3ff501
s121137 <vincent.vansteenhuyse@student.ap.be>
2022-06-05 23:34:03 +0200

    changed Procfile

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+var express = require('express');
+var app = express();
 
-var app = require('express')();
+//set port
 
-const PORT = process.env.PORT || 3000;
+var port = process.env.port || 8080
 
+app.use(express.static(__dirname));
 
+// routes
 
-app.get("", (req, res)=>{
+app.get("./", function(req, res){
     res.render("index");
 });
 
-app.listen(PORT , ()=>{
-    console.log(`App up at port ${PORT}`);
+app.listen(port, function(){
+    console.log("")
 });
